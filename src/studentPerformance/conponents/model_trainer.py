32797895a6253a6314c9bf0678c00e7c97e168ef
Pascal <pascalmoney8270@gmail.com>
2023-06-28 14:34:47 -0700

    studentperformance app

--- src/studentPerformance/conponents/model_trainer.py
+++ src/studentPerformance/conponents/model_trainer.py
@@ -120,7 +120,7 @@ class ModelTrainer:
         except Exception as e:
             raise CustomException(e,sys)
         
-if __name__=="__main__":
+"""if __name__=="__main__":
     obj=DataIngestion()
     train_data,test_data=obj.initiate_data_ingestion()
 
@@ -128,4 +128,4 @@ if __name__=="__main__":
     train_arr,test_arr,_=data_transformation.initiate_data_transformation(train_data,test_data)
     
     modeltrainer=ModelTrainer()
-    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))
+    print(modeltrainer.initiate_model_trainer(train_arr,test_arr))"""
